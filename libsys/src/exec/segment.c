8098a834e17f62461970cf8b2060232a8d5c6065
nick <nick@localhost.(none)>
2009-06-02 11:14:27 -0400

    ELF loader now compiles cleanly

--- libsys/src/exec/segment.c
+++ libsys/src/exec/segment.c
@@ -3,15 +3,15 @@
 #include <lib.h>
 #include <exec.h>
 
-static int elf_null_segment(elf_ph_t *segment);
-static int elf_load_segment(elf_ph_t *segment);
-static int elf_dynm_segment(elf_ph_t *segment);
-static int elf_intr_segment(elf_ph_t *segment);
-static int elf_note_segment(elf_ph_t *segment);
-static int elf_shar_segment(elf_ph_t *segment);
-static int elf_phdr_segment(elf_ph_t *segment);
+static int elf_null_segment(elf_t *header, elf_ph_t *segment);
+static int elf_load_segment(elf_t *header, elf_ph_t *segment);
+static int elf_dynm_segment(elf_t *header, elf_ph_t *segment);
+static int elf_intr_segment(elf_t *header, elf_ph_t *segment);
+static int elf_note_segment(elf_t *header, elf_ph_t *segment);
+static int elf_shar_segment(elf_t *header, elf_ph_t *segment);
+static int elf_phdr_segment(elf_t *header, elf_ph_t *segment);
 
-typedef int (*elf_segment_handler_t) (elf_ph_t *segment);
+typedef int (*elf_segment_handler_t) (elf_t *header, elf_ph_t *segment);
 
 static elf_segment_handler_t elf_segment_handler[] = {
 elf_null_segment,
@@ -24,7 +24,7 @@ elf_phdr_segment
 };
 
 int elf_segment_load(elf_t *header, elf_ph_t *segment) {
-	return elf_segment_handler[segment->p_type](segment);
+	return elf_segment_handler[segment->p_type](header, segment);
 }
 
 static int elf_null_segment(elf_t *header, elf_ph_t *segment) {
@@ -32,7 +32,7 @@ static int elf_null_segment(elf_t *header, elf_ph_t *segment) {
 }
 
 static int elf_load_segment(elf_t *header, elf_ph_t *segment) {
-	u8int *mem_base = segment->p_vaddr;
+	u8int *mem_base = (void*) segment->p_vaddr;
 	u32int file_off = segment->p_offset;
 	u8int *file_base = (u8int*) header;
 	u32int i;
@@ -42,7 +42,7 @@ static int elf_load_segment(elf_t *header, elf_ph_t *segment) {
 		mmap_call(i, (segment->p_flags & PF_W) ? 0x7 : 0x5);
 
 	// Copy data
-	memcpy(mem_base, file_base[file_off], segment->p_memsz);
+	memcpy(mem_base, &file_base[file_off], segment->p_filesz);
 
 	return 0;
 }
