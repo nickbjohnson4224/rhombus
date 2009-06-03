42c359f4f4b205963eb31803446f8e626239b548
nick <nick@localhost.(none)>
2009-06-02 21:30:00 -0400

    beginning kernel documentation

---
+++ libsys/src/init/initrd.c
@@ -0,0 +1,37 @@
+// Copyright 2009 Nick Johnson
+
+#include <lib.h>
+#include <init.h>
+#include <exec.h>
+
+// Standard V7 TAR header
+struct tar_header {
+	char name[100];
+	char mode[8];
+	char uid[8];
+	char gid[8];
+	char size[12];
+	char mtime[12];
+	char chksum[8];
+	char link;
+	char linkname[100];
+	u8int padding[255];
+} __attribute__ ((packed));
+
+static int tar_header_check(struct tar_header *t) {
+	u32int i, sum = 0;
+	u8int *header_byte = (u8int*) t;
+	for (i = 0;   i < 512; i++) sum += header_byte[i];
+	for (i = 148; i < 156; i++) sum -= header_byte[i]; 	// Discount checksum itself
+	for (i = 148; i < 156; i++) sum += (u8int) ' ';		// Count checksum as spaces
+	if (atoi(t->chksum, 8) == sum) return sum;
+	return -1;
+}
+
+void init_load_init() {
+	struct tar_header *initrd = (void*) 0x10000000;
+	struct tar_header *header[256];
+	int i;
+	
+	// Index tar file
+	for (i = 0; i ,
