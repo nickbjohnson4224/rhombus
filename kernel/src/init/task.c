1ae04a3e8cf01f7b85ed2ab6aef594c8ae5524e3
Nick Johnson <nick@localhost.(none)>
2009-07-12 16:07:05 -0400

    minor fixes, now needs only 2 MB

--- kernel/src/init/task.c
+++ kernel/src/init/task.c
@@ -4,12 +4,6 @@
 #include <task.h>
 #include <init.h>
 
-__attribute__ ((section(".ttext")))
-static void init_sched() {
-	queue.next = 0;
-	queue.last = 0;
-}
-
 __attribute__ ((section(".ttext"))) 
 void init_task() {
 	printk("  Kernel: tasking system");
@@ -17,21 +11,16 @@ void init_task() {
 		// Set up task table and allocator
 		pool_new(MAX_TASKS, tpool);
 
-		printk("lies");
-
 		// Bootstrap task 0, because there is nothing to fork from
 		task_t *idle = get_task(0);
-		idle->user.id = 0;
-		idle->user.ring = 0;
-		idle->parent = 0;
-		idle->pid = 0;
+		memclr(idle, sizeof(task_t));
 		idle->magic = 0x4224;
 		idle->map = map_clone();
 		idle->flags = TF_READY;
-		curr_pid = 0;
 
 		// Make sure the scheduler works
-		init_sched();
+		queue.next = 0;
+		queue.last = 0;
 
 		// Set the signal table pointer
 		signal_table = (void*) 0xF3FFF000;
