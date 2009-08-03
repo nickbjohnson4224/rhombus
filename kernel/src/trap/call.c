a5198c8203eb44b01a3844524cef7d31f3140b8f
Nick Johnson <nick@localhost.(none)>
2009-08-03 16:55:16 -0400

    dissolved pit.c

--- kernel/src/trap/call.c
+++ kernel/src/trap/call.c
@@ -4,6 +4,14 @@
 #include <trap.h>
 #include <task.h>
 
+// Handles IRQ 0, and advances a simple counter used as a clock
+image_t *pit_handler(image_t *state) {
+	static u32int tick = 0;
+	tick++;
+
+	return task_switch(next_task(0));
+}
+
 image_t *fault_generic(image_t *image) {
 	if ((image->cs & 0x3) == 0) {
 		printk("EIP:%x NUM:%d ERR:%x\n", image->eip, image->num, image->err);
