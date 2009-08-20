1a299ad30f0266d898decbb650f245380f47c293
Nick Johnson <nick@localhost.(none)>
2009-08-20 13:28:58 -0400

    cleaned up run

--- run/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mount /dev/sda1 /boot
-cp src/kernel /boot/khaos-0.1
-cp ../run/initrd.tar /boot/initrd.tar
-chmod 644 /boot/khaos-0.1
-umount /boot
--- run/runtemp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# runs bochs
-
-/sbin/losetup /dev/loop4 temp.img
-mount /dev/loop4 mnt
-umount /dev/loop4
-bochs -f bochsrc.txt -q
-/sbin/losetup -d /dev/loop4
