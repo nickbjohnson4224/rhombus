46c7d294b53141b23a763068889d77912475f082
Nick Johnson <nick@localhost.(none)>
2009-09-13 13:34:47 -0400

    general line grubbing

--- kernel/init.c
+++ kernel/init.c
@@ -13,8 +13,7 @@
 __attribute__ ((section(".tdata"))) 
 const char *stamp = "\
 Khaos Operating System v%d.%d\n\
-Copyright 2009 Nick Johnson\n\
-===========================\n\n";
+Copyright 2009 Nick Johnson\n\n";
 
 typedef void (*init_t)(void);
 __attribute__ ((section(".tdata")))
@@ -28,13 +27,6 @@ init_free,
 NULL
 };
 
-#ifdef TEST
-init_t test_list[] = {
-test_mem,
-NULL
-};
-#endif
-
 struct multiboot *mboot;
 
 typedef void (*entry_t)();
@@ -42,18 +34,13 @@ typedef void (*entry_t)();
 void init(void *mboot_ptr, uint32_t mboot_magic) {
 	uint32_t i;
 
-	cleark();
+	cleark(); 
 	printk(stamp, KHAOS_VERSION_MAJOR, KHAOS_VERSION_MINOR);
 
 	if (mboot_magic != 0x2BADB002) panic("Bootloader is not multiboot compliant");
 	else mboot = mboot_ptr;
 
 	for (i = 0; init_list[i]; i++) init_list[i]();
-
-	#ifdef TEST
-	for (i = 0; test_list[i]; i++) test_list[i]();	
-	for(;;);
-	#endif
 
 	printk("  Kernel: dropping to usermode");
 	cursek(74, -1);
--- kernel/signal.c
+++ kernel/signal.c
@@ -6,43 +6,32 @@
 
 image_t *signal(pid_t targ, uint8_t sig, 
 	uint32_t arg0, uint32_t arg1, uint32_t arg2, uint32_t arg3, uint8_t flags) {
-
 	task_t *dst_t = task_get(targ); 
 	task_t *src_t = task_get(curr_pid);
-	pid_t caller = curr_pid;
 
-	/* Check existence of target */
-	if (!dst_t) {
-		if (flags & TF_NOERR) return src_t->image;
-		else ret(src_t->image, ENOTASK);
-	}
-
-	/* Switch to target task */
-	task_switch(dst_t);
-
-	/* Block if set to block */
-	if (flags & TF_BLOCK) src_t->flags |= TF_BLOCK;
+	if (!dst_t) ret(src_t->image, (flags & TF_NOERR) ? targ : ENOTASK);	/* Check target */
+	if (flags & TF_BLOCK) src_t->flags |= TF_BLOCK; 					/* Block if set to block */
+	task_switch(dst_t); 												/* Switch to target task */
 
 	/* Create new image structure */
 	memcpy((uint8_t*)  ((uint32_t) dst_t->image - sizeof(image_t)), dst_t->image, sizeof(image_t));
 	dst_t->image = (void*) ((uint32_t) dst_t->image - sizeof(image_t));
 
 	/* Check for task image stack overflows */
-	if ((uintptr_t) dst_t->image < SSTACK_BSE + 2 * sizeof(image_t) && !(flags & TF_SUPER)) 
+	if ((uintptr_t) dst_t->image < SSTACK_BSE + 2 * sizeof(image_t) && !(flags & TF_SUPER))
 		signal(targ, S_IMG, 0, 0, 0, 0, TF_SUPER);
 	if ((uintptr_t) dst_t->image < SSTACK_BSE + sizeof(image_t))
 		return exit_call(dst_t->image);
 
 	/* Set registers to describe signal */
-	dst_t->image->eax = arg0;
-	dst_t->image->ebx = arg1;
-	dst_t->image->ecx = arg2;
-	dst_t->image->edx = arg3;
-	dst_t->image->esi = caller;
-	dst_t->image->edi = sig;
+	dst_t->image->eax = arg0;										/* From EAX */
+	dst_t->image->ebx = arg1;										/* From EBX */
+	dst_t->image->ecx = arg2;										/* From ECX */
+	dst_t->image->edx = arg3;										/* From EDX */
+	dst_t->image->esi = src_t->pid;									/* Caller PID */
+	dst_t->image->edi = sig;										/* Signal number */
 	dst_t->image->ebp = (uintptr_t) dst_t->image + sizeof(image_t); /* Points to saved image */
-	dst_t->image->eip = dst_t->shandler; /* Reentry point */
-
+	dst_t->image->eip = dst_t->shandler; 							/* Reentry point */
 	return dst_t->image;
 }
 
@@ -50,14 +39,9 @@ image_t *sret(image_t *image) {
 	task_t *dst_t = task_get(curr_pid);
 	task_t *src_t = task_get(image->esi);
 
-	/* Unblock the caller */
-	if (src_t && image->eax & TF_UNBLK) src_t->flags &= ~TF_BLOCK;
-
-	/* Reset image stack */
-	dst_t->image = (void*) ((uintptr_t) dst_t->image + sizeof(image_t));
-
-	/* Bounds check image */
-	if ((uint32_t) dst_t->image - sizeof(image_t) >= SSTACK_TOP)
+	if (src_t && image->eax & TF_UNBLK) src_t->flags &= ~TF_BLOCK;			/* Unblock the caller */
+	dst_t->image = (void*) ((uintptr_t) dst_t->image + sizeof(image_t)); 	/* Reset image stack */
+	if ((uint32_t) dst_t->image - sizeof(image_t) >= SSTACK_TOP)			/* Bounds check image */
 		return exit_call(dst_t->image);
 
 	return dst_t->image;
