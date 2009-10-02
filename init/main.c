d8a79de6354307888da6b2046b22ce9e912c52da
Nick Johnson <nick@localhost.(none)>
2009-10-02 16:00:58 -0400

    added simple bootsplash

--- init/main.c
+++ init/main.c
@@ -1,48 +1,8 @@
-#include <lib.h>
 #include <stdint.h>
 #include <kernel.h>
 #include <driver.h>
 #include <exec.h>
-
-static uint16_t video_buf[2000];
-static uint16_t *video_mem = (void*) 0xB8000;
-static int c_base = 0;
-static int cursor = 0;
-static uint8_t attr = 0x0F;
-
-static void sclear(void) {
-	int i;
-	for (i = 0; i < 2000; i++) video_buf[i] = 0x0F20;
-}
-
-static void scroll(void) {
-	int i;
-	for (i = 0; i < 1920; i++) video_buf[i] = video_buf[i + 80];
-    for (i = 1920; i < 2000; i++) video_buf[i] = 0x0F20;
-	c_base -= 80;
-	cursor -= 80;
-}
-
-void cwrite(char c) {
-	if (cursor >= 1999) scroll();
-	switch (c) {
-		case '\0': break;
-		case '\t': cursor = (cursor + 4) - (cursor % 4); break;
-		case '\n': c_base = cursor = (cursor - cursor % 80) + 80; if (cursor>1920) scroll(); break;
-		case '\r': cursor = (cursor - cursor % 80); c_base = cursor; break;
-		case '\b': if (c_base < cursor) cursor--; video_buf[cursor] = 0x0F00; break;
-		default: video_buf[cursor++] = (uint16_t) ((attr << 8) | c); break;
-	}
-	push_call(0, (uint32_t) video_mem, (uint32_t) video_buf, 4000);
-	outb(0x3D4, 15);
-	outb(0x3D5, cursor & 0xFF);
-	outb(0x3D4, 14);
-	outb(0x3D5, cursor >> 8);
-}
-
-void swrite(const char *s) {
-	while (*s != '\0') cwrite(*s++);
-}
+#include "console.h"
 
 void death() {
 	sret_call(3);
@@ -50,19 +10,19 @@ void death() {
 
 void segfault() {
 	uint32_t addr, err, ip;
-	char buffer[10];
+/*	char buffer[10]; */
 
 	asm volatile ("mov %%edx, %0" : "=r" (addr));
 	asm volatile ("mov %%ebx, %0" : "=r" (err));
 	asm volatile ("mov %%eax, %0" : "=r" (ip));
 
 	swrite("\n");
-	swrite("Page Fault at ");
-	swrite(itoa(addr, buffer, 16));
+	swrite("Page Fault");
+/*	swrite(itoa(addr, buffer, 16));
 	swrite(" flags ");
 	swrite(itoa(err, buffer, 16));
 	swrite(" instruction ");
-	swrite(itoa(ip, buffer, 16));
+	swrite(itoa(ip, buffer, 16)); */
 	swrite("\n");
 	exit_call(1);
 }
@@ -113,6 +73,27 @@ int init() {
 	rirq_call(1);
 
 	sclear();
+
+	swrite("\n\n\
+\t\t\t\t\t\t                'a,\n\
+\t\t\t\t\t\t            .     'b.\n\
+\t\t\t\t\t\t          .d'       b.\n\
+\t\t\t\t\t\t          S:        a:\n\
+\t\t\t\t\t\t          'q,       p'\n\
+\t\t\t\t\t\t            '     .p'\n\
+\t\t\t\t\t\t                .a'\n\n\
+\t\t\t\t\t _  _   _   _   _____   _____   _____ \n\
+\t\t\t\t\t| |/ / | |_| | |___  | |  _  | |  ___|\n\
+\t\t\t\t\t|  <   |  _  | |  _  | | |_| | |___  |\n\
+\t\t\t\t\t|_|\\_\\ |_| |_| |_____| |_____| |_____|\n\n\
+\t\t\t\t\t ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,, \n\
+\t\t\t\t\t|                                    |\n\
+\t\t\t\t\t '''''''''''''''''''''''''''''''''''' \n\
+\n");
+
+	update_bar();
+	update_bar();
+	update_bar();
 
 	for(;;);
 	return 0;
