87c2156c67e63b1c8ed54577efaa7173daf42da4
Nick Johnson <nick@localhost.(none)>
2009-11-17 22:18:43 -0500

    prototype iso646 and limits headers

---
+++ libc/inc/limits.h
@@ -0,0 +1,29 @@
+#ifndef LIMITS_H
+#define LIMITS_H
+
+#define CHAR_BIT	8
+#define SCHAR_MIN	0x80
+#define SCHAR_MAX	0x7F
+#define UCHAR_MAX	0xFF
+#define CHAR_MIN	0x80
+#define CHAR_MAX	0x7F
+
+#define MB_LEN_MAX	4
+
+#define SHRT_MIN	0x8000
+#define SHRT_MAX	0x7FFF
+#define USHRT_MAX	0xFFFF
+
+#define INT_MIN		0x80000000
+#define INT_MAX		0x7FFFFFFF
+#define UINT_MAX	0xFFFFFFFF
+
+#define LONG_MIN	0x80000000
+#define LONG_MAX	0x7FFFFFFF
+#define ULONG_MAX	0xFFFFFFFF
+
+#define LLONG_MIN	0x8000000000000000LL
+#define LLONG_MAX	0x7FFFFFFFFFFFFFFFLL
+#define ULLONG_MAX	0xFFFFFFFFFFFFFFFFULL
+
+#endif
