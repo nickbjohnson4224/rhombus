699442501ab2a0da59bb0394f1184d69d00a1f37
Nick Johnson <nickbjohnson4224@gmail.com>
2010-08-21 16:08:08 -0400

    added setjmp.h

---
+++ libc/inc/setjmp.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2009, 2010 Nick Johnson <nickbjohnson4224 at gmail.com>
+ * 
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef SETJMP_H
+#define SETJMP_H
+
+#include <stdint.h>
+
+/* stack context structure **************************************************/
+
+struct __jmp_buf {
+	uintptr_t esp;
+	uintptr_t ebp;
+	uintptr_t eip;
+
+	uintptr_t ebx;
+	uintptr_t edi;
+	uintptr_t esi;
+} __attribute__ ((packed))i;
+
+typedef struct __jmp_buf jmp_buf[1];
+
+/* save stack context *******************************************************/
+
+int setjmp(jmp_buf env);
+
+/* load stack context *******************************************************/
+
+int longjmp(jmp_buf env, int val);
+
+#endif/*SETJMP_H*/
---
+++ libc/setjmp/setjmp.s
@@ -0,0 +1,34 @@
+; Copyright (C) 2009, 2010 Nick Johnson <nickbjohnson4224 at gmail.com>
+; 
+; Permission to use, copy, modify, and/or distribute this software for any
+; purpose with or without fee is hereby granted, provided that the above
+; copyright notice and this permission notice appear in all copies.
+; 
+; THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+; WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+; MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+; ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+; WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+; ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+; OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+[bits 32]
+
+global setjmp
+
+setjmp:
+	mov eax, [esp+4]
+
+	pop ecx
+	
+	mov [eax+0x00], esp
+	mov [eax+0x04], ebp
+	mov [eax+0x08], ecx
+	mov [eax+0x0C], ebx
+	mov [eax+0x10], edi
+	mov [eax+0x14], esi
+
+	xor eax, eax
+
+	push ecx
+	ret
