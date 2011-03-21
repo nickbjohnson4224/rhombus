88334204dd37c7d8999d5abd3fe473e4365c172b
Nick Johnson <nickbjohnson4224@gmail.com>
2011-03-21 19:05:38 -0400

    added jrepan's qsort patch

--- libc/stdlib/qsort.c
+++ libc/stdlib/qsort.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2009, 2010 Nick Johnson <nickbjohnson4224 at gmail.com>
+ * Copyright (C) 2011 Jaagup Repän
  * 
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -15,19 +16,55 @@
  */
 
 #include <stdlib.h>
-#include <errno.h>
+#include <string.h>
 
 /****************************************************************************
  * qsort
  *
- * Sorts the array <base> containing <n> elements of size <size> in
+ * Sorts the array <base> containing <nmemb> elements of size <size> in
  * ascending order based on <cmp>.
- *
- * XXX - dummy (does anyone actually use this function?)
  */
 
-void qsort(void *base, size_t n, size_t size,
-		int (*cmp)(const void *keyval, const void *datum)) {
-	
-	errno = ENOSYS;
+void qsort(void *base, size_t nmemb, size_t size, int(*compar)(const void *, const void*)) {
+	if (nmemb <= 1) {
+		return;
+	}
+
+	size_t left = 0;
+	size_t right = nmemb - 1;
+	void *pivot = malloc(size);
+	void *t = malloc(size);
+	memcpy(pivot, (uint8_t*) base + (left + right) / 2 * size, size);
+
+	while (1) {
+		// Find items to swap
+		while (left < nmemb && compar((uint8_t*) base + left * size, pivot) < 0) {
+			left++;
+		}
+		while (right > 0 && compar((uint8_t*) base + right * size, pivot) > 0) {
+			right--;
+		}
+
+		if (left >= right) {
+			// all items are divided
+			break;
+		}
+		if (compar((uint8_t*) base + left * size, (uint8_t*) base + right * size) == 0) {
+			// skip equal items
+			left++;
+			continue;
+		}
+
+		// ... and swap them
+		memcpy(t, (uint8_t*) base + right * size, size);
+		memcpy((uint8_t*) base + right * size, (uint8_t*) base + left * size, size);
+		memcpy((uint8_t*) base + left * size, t, size);
+	}
+
+	free(pivot);
+	free(t);
+
+	// sort items less than pivot and greater than pivot recursively
+	qsort(base, left, size, compar);
+	qsort((uint8_t*) base + (left + 1) * size, nmemb - left - 1, size, compar);
 }
