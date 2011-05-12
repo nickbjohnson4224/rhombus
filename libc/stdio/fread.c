40e97a3d7ce74ecbba593869559eeddd8951e40f
Jaagup Repän <jrepan@gmail.com>
2011-05-12 20:12:01 +0300

    bugfixes in fread(), vfscanf() and in vsscanf()

--- libc/stdio/fread.c
+++ libc/stdio/fread.c
@@ -49,8 +49,9 @@ size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream) {
 		rev_count = 0;
 	}
 
-	ret = read(stream->fd, ptr, nmemb - rev_count, stream->position) + rev_count;
+	ret = read(stream->fd, ptr, nmemb - rev_count, stream->position);
 	stream->position += ret;
+	ret += rev_count;
 
 	if (ret != nmemb) {
 		stream->flags |= FILE_EOF;
