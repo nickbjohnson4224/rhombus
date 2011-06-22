5637b3307ec3a07805472e5b99a13078ddddcfd8
Nick Johnson <nickbjohnson4224@gmail.com>
2011-06-21 22:33:53 -0400

    improved hash implementation

--- libc/vfs/id_hash.c
+++ libc/vfs/id_hash.c
@@ -22,31 +22,63 @@
 
 void id_hash_set(struct id_hash *h, uint32_t id, uint32_t value) {
 	struct id_hash_list *node;
+	int test = id_hash_test(h, id);
 
 	mutex_spin(&h->mutex);
+	if (value == h->nil) {
+		/* remove id if it exists (set it to nil) */
+		node = h->table[id % h->size];
 
-	if (h->size == 0) {
-		h->size = HASHSIZE;
-		h->table = calloc(sizeof(struct id_hash_list*), HASHSIZE);
+		while (node) {
+			if (node->id == id) {
+				break;
+			}
+			node = node->next;
+		}
+
+		if (node) {
+			if (node->next) node->next->prev = node->prev;
+			if (node->prev) node->prev->next = node->next;
+			else h->table[id % h->size] = node->next;
+		}
 	}
+	else if (test) {
+		/* id exists, modify entry */
+		node = h->table[id % h->size];
 
-	node = malloc(sizeof(struct id_hash_list));
-	node->id    = id;
-	node->value = value;
+		while (node) {
+			if (node->id == id) {
+				break;
+			}
+			node = node->next;
+		}
 
-	id = id % h->size;
-	node->next = h->table[id];
-	node->prev = NULL;
-	h->table[id] = node;
+		node->value = value;
+	}
+	else {
+		/* id does not exist, create entry */
+		if (h->size == 0) {
+			h->size = HASHSIZE;
+			h->table = calloc(sizeof(struct id_hash_list*), HASHSIZE);
+		}
 
+		node = malloc(sizeof(struct id_hash_list));
+		node->id    = id;
+		node->value = value;
+
+		id = id % h->size;
+		node->next = h->table[id];
+		node->prev = NULL;
+		h->table[id] = node;
+	}
 	mutex_free(&h->mutex);
 }
 
 uint32_t id_hash_get(struct id_hash *h, uint32_t id) {
 	struct id_hash_list *node;
+	uint32_t value;
 	
 	mutex_spin(&h->mutex);
-
 	if (h->size == 0) {
 		mutex_free(&h->mutex);
 		return h->nil;
@@ -61,24 +93,19 @@ uint32_t id_hash_get(struct id_hash *h, uint32_t id) {
 		node = node->next;
 	}
 
+	value = (node) ? node->value : h->nil;
 	mutex_free(&h->mutex);
 
-	if (!node) {
-		return h->nil;
-	}
-	else {
-		return node->value;
-	}
+	return value;
 }
 
 int id_hash_test(struct id_hash *h, uint32_t id) {
 	struct id_hash_list *node;
 	
 	mutex_spin(&h->mutex);
-
 	if (h->size == 0) {
 		mutex_free(&h->mutex);
-		return 0;
+		return h->nil;
 	}
 
 	node = h->table[id % h->size];
@@ -89,16 +116,16 @@ int id_hash_test(struct id_hash *h, uint32_t id) {
 		}
 		node = node->next;
 	}
-
 	mutex_free(&h->mutex);
 
-	if (!node) {
-		return 0;
-	}
-	else {
-		return 1;
-	}
+	return (node) ? 1 : 0;
 }
+
+/*****************************************************************************
+ * id_hash_count
+ *
+ * Returns the number of non-nil elements in <h>.
+ */
 
 int id_hash_count(struct id_hash *h) {
 	int count;
@@ -110,8 +137,15 @@ int id_hash_count(struct id_hash *h) {
 	return count;
 }
 
+/*****************************************************************************
+ * id_hash_free
+ *
+ * Frees all memory associated with <h>.
+ */
+
 void id_hash_free(struct id_hash *h) {
 	
+	mutex_spin(&h->mutex);
 	if (h->table) {
 		free(h->table);
 	}
